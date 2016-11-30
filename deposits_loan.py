76b903701be6869b9bff9eff585c831ca62ca983
Amiya Behera <amiyatulu@gmail.com>
2016-11-30 13:16:18 +0530

    Update deposits_loan.py
    Read from csv

--- deposits_loan.py
+++ deposits_loan.py
@@ -1,4 +1,5 @@
 import simpy
+import csv
 
 class Person_Transaction(object):
     def __init__(self, env, name, money_container, time_of_deposit, time_of_withdraw, amount_deposited, amount_withdrawn):
@@ -15,17 +16,19 @@ class Person_Transaction(object):
     def run(self):
         yield self.env.timeout(self.time_of_deposit)
         yield self.money_container.put(self.amount_deposited)
-        print("%s deposited at time %d" % (self.name, self.env.now))
-        print("container level after %s deposited is % d" % (self.name, self.money_container.level))
+        print("%s deposited %d at time %d" % (self.name, self.amount_deposited, self.env.now))
+        print("amount left in container is % d at time %d" % (self.money_container.level, self.env.now))
         yield self.env.timeout(self.wait_time)
         yield self.money_container.get(self.amount_withdrawn)
-        print("%s withdrawn at time %d" % (self.name, self.env.now))
-        print("container level after %s has withdrawn is % d" % (self.name, self.money_container.level))
+        print("%s withdrawn %d at time %d" % (self.name, self.amount_withdrawn, self.env.now))
+        print("amount left in container is % d at time %d" % (self.money_container.level, self.env.now))
 
 
 if __name__ == "__main__":
     env = simpy.Environment()
     cont = simpy.Container(env)
-    person1 = Person_Transaction(env, name="Amiya", money_container=cont, time_of_deposit=2, time_of_withdraw=4, amount_deposited=50, amount_withdrawn=40)
-    person2 = Person_Transaction(env, name="Sourav", money_container=cont, time_of_deposit=3, time_of_withdraw=4, amount_deposited=100, amount_withdrawn=500)
-    env.run(until=15)
+    with open("data.csv", newline="") as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            x = Person_Transaction(env, name= row["Name"], money_container=cont, time_of_deposit=int(row["Time of Deposit"]), time_of_withdraw=int(row["Time of Withdraw"]), amount_deposited=int(row["Amount Deposited"]), amount_withdrawn=int(row["Amount Withdrawn"]))
+    env.run(until=20)
